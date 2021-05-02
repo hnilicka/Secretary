34c35d5bbd7a51ca9e79f4ae3c9427d05bab99b8
Roman Hnilicka <romanhnilicka@centrum.cz>
2021-05-02 09:42:58 +0200

    20210502 StudijniSkupina datova knihovna

---
+++ SecretaryLibrary/SkupinaModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SecretaryLibrary
+{
+    public class SkupinaModel
+    {
+        // datova knihovna Studijni skupiny; Studijni skupna se rozdeluje do pracovnich stitku.
+        // Pocet studentu je nutne manualne vlozit nejsou automaticky napocteny ze seznanu List<Predmet>
+
+        public SkupinaModel()
+        {
+        }
+
+        public string Zkratka { get; set; }
+        public string Nazev { get; set; }
+        public int Rocnik { get; set; }
+        // enum dava vyctum hodnot cisla od 0
+        // priklad LS = 0; ZS = 1; 
+        public enum Semestr {LS, ZS}
+        public int PocetStudentu { get; set; }
+        public enum FormaStudia { P,K}
+        public enum TypStudia { Bc, Ing }
+        public enum Jazyk { cz, en}
+        // public List <Predmet>
+
+    }
+}
