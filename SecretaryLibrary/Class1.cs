297d09fb8b3bd832a3353cb029fe1407ce5487cd
Roman Hnilicka <romanhnilicka@centrum.cz>
2021-06-30 20:40:13 +0200

    vahymodel

---
+++ SecretaryLibrary/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretaryLibrary;
+{
+
+	public class vahymodel
+    {
+    public vahymodel()
+    {
+    }
+    public double HodPrednaska { get; set; }
+    public double HodCviceni { get; set; }
+    private double HodSeminar { get; set; }
+    private double HodPrednaskaEn { get; set; }
+    private double HodCviceniEn { get; set; }
+    private double HodSeminarEn { get; set; }
+    private double UdelitZapocet { get; set; }
+    private double UdelitKlas { get; set; }
+    private double UdelitZkouska { get; set; }
+    private double UdelitZapocetEn { get; set; }
+    private double UdelitKlasEn { get; set; }
+    private double UdelitZkouskaEn { get; set; }
+
+    }
+}
